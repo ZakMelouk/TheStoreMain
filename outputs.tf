e696a27bae55177921ff6dd3af7c9ad26f3d5519
ZakMelouk <129533388+ZakMelouk@users.noreply.github.com>
2025-11-05 19:56:04 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -33,19 +33,47 @@ output "catalog_db_secret_arn" {
   value       = aws_secretsmanager_secret.catalog_db_secret.arn
 }
 
-output "catalog_db_name" {
-  description = "Database name for Catalog MySQL"
-  value       = var.catalog_db_name
+output "redis_endpoint" {
+  description = "Primary endpoint for Checkout Redis"
+  value       = aws_elasticache_cluster.checkout.cache_nodes[0].address
 }
 
-output "catalog_db_username" {
-  description = "Database username for Catalog MySQL"
-  value       = var.catalog_db_username
+output "redis_port" {
+  description = "Port for Checkout Redis"
+  value       = aws_elasticache_cluster.checkout.port
+}
+output "cart_dynamodb_table_name" {
+  description = "Name of the DynamoDB table for Cart service"
+  value       = aws_dynamodb_table.cart_items.name
+}
+output "dynamodb_vpc_endpoint_id" {
+  description = "ID of the VPC Endpoint for DynamoDB"
+  value       = aws_vpc_endpoint.dynamodb.id
+}output "vpc_id" {
+  description = "Main VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+}
+
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
 
 output "catalog_secret_arn" {
   value       = aws_secretsmanager_secret.catalog_db_secret.arn
   description = "ARN du secret Catalog"
 }
+output "k8s_master_private_ip" {
+  description = "Private IP of the Kubernetes master node"
+  value       = aws_instance.k8s_master.private_ip
+}
 
-
+output "k8s_worker_private_ips" {
+  description = "Private IPs of the Kubernetes worker nodes"
+  value       = [for w in aws_instance.k8s_worker : w.private_ip]
+}
